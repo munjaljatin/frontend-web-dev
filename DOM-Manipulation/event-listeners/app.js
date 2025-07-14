a37c558154f8c757810c23077e6bf87584d37ce4
Jatin Munjal <munjal.jatin24@gmail.com>
2025-07-14 10:34:09 +0530

    DOM Event Listeners started

---
+++ DOM-Manipulation/event-listeners/app.js
@@ -0,0 +1,5 @@
+const heading3 = document.querySelector(".heading3");
+heading3.addEventListener("click", () => {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+});
