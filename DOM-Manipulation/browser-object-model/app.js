ce56a417eabe005eeb90688c7ea1bccba43344c2
Jatin Munjal <munjal.jatin24@gmail.com>
2025-07-18 22:17:40 +0530

    DOM updated

--- DOM-Manipulation/browser-object-model/app.js
+++ DOM-Manipulation/browser-object-model/app.js
@@ -6,3 +6,11 @@ console.log(heading2);
 console.dir(heading2);
 
 heading2.innerText = "Hello";
+
+const innerH = window.innerHeight;
+const outerH = window.outerHeight;
+const titleBarHeight = outerH - innerH;
+console.log(titleBarHeight);
+
+const name = "jatin";
+console.log(name);
--- DOM-Manipulation/create-element/app.js
+++ DOM-Manipulation/create-element/app.js
@@ -14,14 +14,38 @@ newImageContainer.style.border = "2px solid red";
 console.log(newImageContainer);
 newImageContainer.appendChild(imageElement);
 
-for (let i = 2; i < 1001; i = i + 10) {
+for (let i = 2; i < 100; i = i + 10) {
   const newImage = image.cloneNode();
   newImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/${i}.png`;
   container.append(newImage);
 }
 
-// for (let i = 3; i < 10000; i + 30) {
+// for (let i = 2; i < 1000; i + 30) {
 //   const newImage2 = imageElement.cloneNode();
 //   newImage2.src = `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/${i}.png`;
 //   newImageContainer.append(newImage2);
 // }
+
+// Card Container
+const cardContainer = document.querySelector(".card-container");
+console.log(cardContainer);
+console.dir(cardContainer);
+
+const paraElement = document.createElement("p");
+console.log(paraElement);
+
+paraElement.innerText =
+  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita aspernatur veniam culpa quaerat? Aut ad iusto nam repudiandae nobis placeat reiciendis! Eos odio sit perferendis nulla dolor expedita voluptatem corporis?";
+
+cardContainer.appendChild(paraElement);
+
+// Main Container
+const mainContainer = document.querySelector(".main-container");
+console.log(mainContainer);
+
+const mainParagraph = document.createElement("p");
+mainParagraph.innerHTML = "<b>This is Jatin Munjal</b>";
+// mainParagraph.innerText = "He is a boy.";
+// mainParagraph.textContent = "She is a girl.";
+console.log(mainParagraph.textContent);
+mainContainer.appendChild(mainParagraph);
